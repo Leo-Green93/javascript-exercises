ea95b191617337546b4859d8f8a3830fe7ccb493
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-26 17:09:41 -0400

    Solutions to JavaScript Exercises
    This commit includes solutions for exercises 1 and 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let newString = ""
+if (num > 0) {
+for (let i = num; i > 0; i--) {
+    newString += string
+    num-= 1;
+    
+} return newString;
+} else if (num < 0) {
+    newString = "ERROR"
+    return newString;
+} else {
+    return ""
+}
+}
 
-};
+
+/* while (num > 0) {
+    newString += string;
+    num -= 1;   
+} 
+return newString; */
+
 
 // Do not edit below this line
 module.exports = repeatString;
