74ae6ef7dd0ac1494f2021a23b61a8a090b9e12b
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-27 10:30:12 -0400

    Upload solution to palindromes and fibonacci tasks

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+        str = str.toLowerCase()
+       .replace(/[^a-z0-9]/gi, '')
+       .split("")
+     
+     let newArray = [];
+     
+     for (let i = str.length - 1; i >= 0; i--) {
+      newArray.push(str[i])
+     
+     }
+     //console.log(str, newArray)
+     
+     for (let i = 0; i <= str.length - 1; i++) {
+         if (str[i] !== newArray[i]) {
+           return false
+         }
+       }
+       return true
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    if(count < 0) {return "OOPS"};
+    if(count === 0) {return 0};
+
+    let fibOne = 1;
+    let fibTwo = 0;
+
+    for(let i = 2; i <= count; i++) {
+        let fibCurrent = fibOne + fibTwo;
+        fibTwo = fibOne;
+        fibOne = fibCurrent;
+    } return fibOne
 
 };
 
