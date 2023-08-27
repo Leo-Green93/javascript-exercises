8809d20a8bc6731584ac32df5be9d10ebcfe0e6d
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-27 13:58:22 -0400

    Upload solutions to getTheTitles and findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestPerson = yearsOld(oldest.yearOfBirth, oldest.yearOfDeath)
+        const currentPersonAge = yearsOld(
+            currentPerson.yearOfBirth, 
+            currentPerson.yearOfDeath
+        );
+        return oldestPerson < currentPersonAge ? currentPerson : oldest;
+     });
+    }
 
-};
+    const yearsOld = function(yearOfBirth, yearOfDeath) {
+        if (!yearOfDeath) {
+            yearOfDeath = new Date().getFullYear();
+        } 
+        return yearOfDeath - yearOfBirth;
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
