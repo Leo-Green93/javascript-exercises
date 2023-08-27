08fd1dca4a285b3739c473136d9fd1e9d2c762c9
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-26 21:45:19 -0400

    Upload solution for calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+  sum += arr[i]
+  } return sum
+
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+let result = 1;
+for (let i = 0; i < arr.length; i++) {
+result *= arr[i]
+}
+  return result
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1
+  if (num === 0) {
+    return 1
+  } else {
+    for (let i = num; num > 0; num--)
+    factorial *= num 
+  } return factorial;
 };
 
 // Do not edit below this line
