fbe6edbb359f14ab01d4fdb471618ae1f0e02f56
Leo-Green93 <139697327+Leo-Green93@users.noreply.github.com>
2023-08-26 21:04:46 -0400

    Add solutions for exercises 5 and 6
    Upload solutions for leapYears and tempConversions exercises.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsiusTemp = 0;
+  celsiusTemp += ((temp - 32) * 5) / 9
+  return Math.round(celsiusTemp * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farenheitTemp = 0;
+  farenheitTemp = temp * 1.8 + 32;
+  return Math.round(farenheitTemp * 10) / 10
 };
 
 // Do not edit below this line
